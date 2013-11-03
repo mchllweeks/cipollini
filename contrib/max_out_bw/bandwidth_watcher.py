949dc518ba2a1dd2792e12c7d4cfaccdf709e29a
mchllweeks <michelle.schuknecht@gmail.com>
2013-11-02 19:20:29 -0700

    file upload working, other cleanup

--- contrib/max_out_bw/bandwidth_watcher.py
+++ contrib/max_out_bw/bandwidth_watcher.py
@@ -1,11 +1,24 @@
 import requests
+import time
 from random import choice
 from tempfile import TemporaryFile
+
+class Timer:
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
+
 
 class BandwidthWatcher:
     # this will eventually need a list of speed test
     # sites we can use
-    server_list = []
+    server_list = ['http://httpbin.org/', 'http://eu.httpbin.org/', 'http://www.posttestserver.com/',
+                   'https://posttestserver.com/post.php', 'http://greensuisse.zzl.org/product/dump/dump.php',
+                   'http://www.newburghschools.org/testfolder/dump.php']
     max_bw = None
     min_bw = None
 
@@ -24,8 +37,11 @@ class BandwidthWatcher:
         @param file_object file like object
         @return int average transfer rate
         """
-        upload = requests.post(server, data=file_object)
-        return upload.content
+        files = {'file': open(file_object, 'rb')}
+        with Timer() as t:
+            p = requests.post(server, files=files)
+            p.content
+        return t.interval
 
     @staticmethod
     def random_list_subset(lst, size):
@@ -50,7 +66,7 @@ class BandwidthWatcher:
         f = open('dev/urandom')
         f.read(size)
         tempfile = TemporaryFile()
-        f.write(tempfile)
+        return f.write(tempfile)
 
     def file_upload_test(self, file_size, list_size):
         """
@@ -59,7 +75,7 @@ class BandwidthWatcher:
         @return list returns a list of transfer rates
         """
         f = self.get_file_object(file_size)
-        servers = self.random_list_subset(self.server_list, size)
+        servers = self.random_list_subset(self.server_list, list_size)
         transfer_rates = []
         for server in servers:
             transfer_rates.append(self.file_upload(server, f))
@@ -70,34 +86,13 @@ class BandwidthWatcher:
         @return list list of averages of all multiple file_upload_test calls
         """
         list_of_averages = []
-        try1_small_file = self.file_upload_test(314572,5)
-        for x in try1_small_file:
-            sum_try1 = x + sum_try1
-        try1_avg = sum_try1 / len(try1_small_file)
-        list_of_averages.append(try1_avg)
 
-        try2_small_file = self.file_upload_test(314572,5)
-        for x in try2_small_file:
-            sum_try2 = x + sum_try2
-        try2_avg = sum_try2 / len(try2_small_file)
-        list_of_averages.append(try2_avg)
-
-        try3_small_file = self.file_upload_test(314572, 5)
-        for x in try3_small_file:
-            sum_try3 = x + sum_try3
-        try3_avg = sum_try3 / len(try3_small_file)
-        list_of_averages.append(try3_avg)
-
-        try1_large_file = self.file_upload_test(2097152, 5)
-        for x in try1_large_file:
-            sum_try1 = x + sum_try1
-        try1_avg = sum_try1 / len(try1_large_file)
-        list_of_averages.append(try1_avg)
-
-        try2_large_file = self.file_upload_test(2097152, 5)
-        for x in try1_large_file:
-            sum_try2 = x + sum_try2
-        try2_avg = sum_try2 / len(try2_large_file)
-        list_of_averages.append(try2_avg)
+        for x in range(1, 3):
+            x = x + 1
+            try_small_file = self.file_upload_test(314572,5)
+            for y in try_small_file:
+                sum_try = y + sum_try
+            try_avg = sum_try / len(try_small_file)
+            list_of_averages.append(try_avg)
 
         return list_of_averages
